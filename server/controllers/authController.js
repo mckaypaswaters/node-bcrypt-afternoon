a42282fecdcc0cec79cc85a459236a3001337cf6
McKay Waters <mckaypaswaters@gmail.com>
2019-09-25 22:41:39 -0600

    finished step 3 login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,5 +17,22 @@ module.exports = {
 
         req.session.user = {isAdmin: user.is_admin, id: user.id, username: user.username}
         res.status(201).send(req.session.user)
+    },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {username, password} = req.body
+
+        const foundUser = await db.get_user([username])
+        const user = foundUser[0]
+
+        if (!user){
+            return res.status(401).send('User not found. Please register as a new user before logging in')
+        }
+        const isAuthenticated = bcrypt.compareSync(password, user.hash)
+        if (isAuthenticated === false){
+            return res.status(403).send('Incorrect password')
+        }
+        req.session.user = {isAdmin: user.is_admin, id: user.id, username: user.username}
+        return res.send(req.session.user)
     }
 }
